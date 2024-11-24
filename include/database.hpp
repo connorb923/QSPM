d7c4e8cf8b586ab41b7b060eaeaf6b1d26582a6a
pyluadotcode (Connor) <connorbieszk923@gmail.com>
2024-11-24 11:49:20 -0600

    Recode prep

--- include/database.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef DATABASE_H
-#define DATABASE_H
-
-#include "main.hpp"
-
-bool doesFileExist(const std::string& filename);
-int initDB(const std::string& dbFilename);
-
-#endif
--- include/main.hpp
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-
-#include <iostream>
-#include <fstream>
-#include <ctime>
-#include "../lib/sqlite3/sqlite3.h"
-
-extern sqlite3* db;
